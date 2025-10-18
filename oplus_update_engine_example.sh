475208138f20f934c06575d0d6effbbd6d799301
hamjin <jinham@qq.com>
2025-10-19 01:05:11 +0800

    Add oplus update_engine example
    Add oplus update_engine example for downgrade.

---
+++ oplus_update_engine_example.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+
+update_engine_client --payload=file:///data/oplus_ota_package/payload.bin --update --headers="FILE_HASH=XCauYU2tkCbTi+VS5a4Y8+y3SC6cvsM2WJyc1dmBwPQ=
+FILE_SIZE=7877381917
+METADATA_HASH=TLhKjtH767Ga5htXOXP2oKoG2XhWu+MTvSTnfkO9YBY=
+METADATA_SIZE=278498
+android_version=15
+forbid_ota_local_update=false
+ota_root_or_debug=false
+oplus_update_engine_verify_disable=1
+ota_target_version=PLC110_11.A.22_0220_202508291928
+oplus_rom_version=V15.0.2
+security_patch=2025-09-01
+security_patch_vendor=2025-09-01
+oplus_separate_soft=24813" --metadata="/data/oplus_ota_package/META-INF/com/android/metadata" --oplus_switch_slot
